723a1a086b5513548b887fa55cf30231ec3a1a2f
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-09-20 09:35:17 +0700

    feat: implement activation and recharge processors
    Add ActivationProcessor and RechargeProcessor classes to handle specific processing for VCR/VF and mobile number categories respectively. Introduce a ProcessorFactory for creating appropriate processors based on category type. Enhance response processing capabilities with filtering and quota optimization.

--- app/custom/log_utils.py
+++ app/custom/log_utils.py
@@ -1,6 +1,7 @@
 """loguru utility methode."""
 
 # decorator for calculating execution time of functions
+import functools
 from collections.abc import Callable
 from functools import wraps
 from time import perf_counter
@@ -13,6 +14,42 @@ F = TypeVar("F", bound=Callable)
 
 def log_execution_time(func: F) -> F:
     """Decorator to log the execution time of a function."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = perf_counter()
+        result = func(*args, **kwargs)
+        end_time = perf_counter()
+        execution_time = end_time - start_time
+        logger.info(f"Execution time of {func.__name__}: {execution_time:.4f} seconds")
+        return result
+
+    return wrapper  # type: ignore
+
+
+def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
+    def wrapper(func):
+        name = func.__name__
+
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            logger_ = logger.opt(depth=1)
+            if entry:
+                logger_.log(
+                    level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs
+                )
+            result = func(*args, **kwargs)
+            if exit:
+                logger_.log(level, "Exiting '{}' (result={})", name, result)
+            return result
+
+        return wrapped
+
+    return wrapper
+
+
+def timeit(func: F) -> F:
+    """Decorator to measure the execution time of a function."""
 
     @wraps(func)
     def wrapper(*args, **kwargs):
